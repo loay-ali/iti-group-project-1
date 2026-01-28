a47fa4f48ef85dabc608c6b72d2d1aa2801d6612
Loay Ali <144592929+loay-ali@users.noreply.github.com>
2026-01-28 19:32:57 +0200

    Finished Cart & Some Api Server Enhancements

--- dummyServer/index.js
+++ dummyServer/index.js
@@ -1,16 +1,18 @@
 const agent = require('express');
 const server = agent();
 
+server.use((require('cors'))());
+
 const data = require('./product.json');
 
 //All Products
 server.get('/',(req,res) => {
 	if( req.query['category'] === undefined ) {
-		res.send({count: data.length,data: data});
+		res.send({count: data.length,data: data.products});
 	}else {
 		if( /^[a-z]{1,15}$/i.test(req.query['category']) ) {
 			const d = data.products.filter(product => product.category === req.query['category']);
-			res.send({count: d.length,data: d});
+			res.send({count: d.length,data: d.products});
 		}
 		else {
 			res.sendStatus(403);
--- scripts/main.js
+++ scripts/main.js
@@ -38,3 +38,96 @@ const closeCart = () => {
 
 document.getElementById('close-cart-panel').addEventListener('click',closeCart);
 cartPanel.getElementsByClassName('layout')[0].addEventListener('click',closeCart);
+
+//Dynamic Cart
+const updateCartCounter = () => cartCounter.dataset['count'] = Object.keys(getCart()).length;
+
+function getCart() {
+	const data = JSON.parse(localStorage.getItem('cart'));
+
+	return data == null ? []:data;
+}
+
+document.body.addEventListener('click',event => {
+	if( event.target.classList.contains('add-to-cart') ) {
+		const cart = getCart();
+		const productId = event.target.dataset['id'];
+		cart[productId] = ~~(cart[productId]) + 1;
+	
+	localStorage.setItem('cart',JSON.stringify(cart));
+	}
+});
+
+//Get Data
+const cartAjaxAgent = new XMLHttpRequest();
+cartAjaxAgent.open("GET","http://localhost:8000");
+cartAjaxAgent.send();
+
+const cartCounter = document.getElementById('open-cart-panel');
+
+const emptyCartCond = document.getElementById('empty-cart');
+const cartList = document.getElementById('cart-products-table');
+const cartTotals = document.getElementById('cart-totals');
+
+function fillCart(data) {
+	const currentCart = getCart();
+
+	if( Object.keys(currentCart).length == 0 ) {
+		emptyCartCond.style.display = 'block';
+		cartList.style.display = 'none';
+	
+		return;
+	}
+
+	let totaCart = 0;
+	cartList.innerHTML = '';
+
+	for( let product in currentCart ) {
+		const productData = data.filter(prod => prod.id == product)[0];
+		const tr = document.createElement('tr');
+
+		const dataField = document.createElement('td');
+		
+		const img = document.createElement('img');
+		img.src = productData.image;
+		img.alt = productData.name + " Image";
+		img.width = 200;
+
+		const brand = document.createElement('small');
+		brand.innerText = productData.brand;
+		brand.className = 'text-primary';
+
+		const title = document.createElement('span');
+		title.innerText = productData.name;
+		
+		const price = document.createElement('span');
+		price.innerText = "$"+ productData.price;
+
+		dataField.appendChild(img);
+		dataField.appendChild(brand);
+		dataField.appendChild(title);
+		dataField.appendChild(price);
+
+		tr.appendChild(dataField);
+
+		const total = document.createElement('td');
+		total.innerText = "$"+ (productData.price * currentCart[product]);
+
+		tr.appendChild(total);
+
+		totaCart += (productData.price * currentCart[product]);
+
+		cartList.appendChild(tr);
+	}
+
+	cartTotals.innerText = '$'+ totaCart + ' USD';
+}
+
+cartAjaxAgent.addEventListener('readystatechange',() => {
+	if( cartAjaxAgent.readyState == 4 ) {
+		fillCart(JSON.parse(cartAjaxAgent.response).data);
+	}
+});
+
+//Initial Cart Counter
+window.addEventListener('load',updateCartCounter);
